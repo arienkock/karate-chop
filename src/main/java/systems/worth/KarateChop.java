f9ba21d2da1d372b320fb9be413d246254f571cb
Arien Kock <arien@positor.nl>
2018-09-27 15:29:17 +0200

    boilerplate

---
+++ src/main/java/systems/worth/KarateChop.java
@@ -0,0 +1,8 @@
+package systems.worth;
+
+public class KarateChop {
+    public static int search(int target, int[] array) {
+        int result = -1;
+        return result;
+    }
+}
---
+++ src/test/java/systems/worth/KarateChopTest.java
@@ -0,0 +1,10 @@
+package systems.worth;
+
+import static org.junit.Assert.*;
+
+public class KarateChopTest {
+
+    @org.junit.Test
+    public void search() {
+    }
+}
