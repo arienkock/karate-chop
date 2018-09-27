cd09525ef5507ac7c25fbeaa40540380b9e18e43
Arien Kock <arien@positor.nl>
2018-09-27 15:37:41 +0200

    first case

--- src/main/java/systems/worth/KarateChop.java
+++ src/main/java/systems/worth/KarateChop.java
@@ -3,6 +3,16 @@ package systems.worth;
 public class KarateChop {
     public static int search(int target, int[] array) {
         int result = -1;
+        int start = 0;
+        int limit = array.length;
+        while (limit - start > 0) {
+            int middleIndex = (limit + start) / 2;
+            int middleValue = array[middleIndex];
+            if (middleValue == target) {
+                result = middleIndex;
+                break;
+            }
+        }
         return result;
     }
 }
--- src/test/java/systems/worth/KarateChopTest.java
+++ src/test/java/systems/worth/KarateChopTest.java
@@ -6,5 +6,14 @@ public class KarateChopTest {
 
     @org.junit.Test
     public void search() {
+//        assertEquals(KarateChop.search(1, new int[] {0,1,2,3,4}), 1);
+//        assertEquals(KarateChop.search(9, new int[] {0,1,2,3,4}), -1);
+//        assertEquals(KarateChop.search(0, new int[] {0,1,2,3,4}), 0);
+//        assertEquals(KarateChop.search(4, new int[] {0,1,2,3,4}), 4);
+//        assertEquals(KarateChop.search(3, new int[] {0,1,2,3,4}), 3);
+//
+//        assertEquals(KarateChop.search(3, new int[0]), -1);
+//        assertEquals(KarateChop.search(3, new int[] {0,1,2,3}), 3);
+        assertEquals(KarateChop.search(0, new int[] {0}), 0);
     }
 }
